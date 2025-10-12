b8494ba8dfeddfd569cf2546490683e5db376d2b
pr4gmat <orpheus.blackwood@mail.ru>
2025-10-12 18:40:44 +0700

    Add components for the module

---
+++ libs/ui/GTrack.Dialog.Module/Views/DialogMessageView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace GTrack.Dialog.Module.Views;
+
+public partial class DialogMessageView : UserControl
+{
+    public DialogMessageView()
+    {
+        InitializeComponent();
+    }
+}
